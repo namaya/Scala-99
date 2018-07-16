07df61d73ad188290a1d4b32f5fdfeb6a202ffc6
Nick Amaya <namaya@sparkcognition.com>
2018-07-16 12:10:29 -0500

    P5: Implemented reverse using a functional approach.

--- src/main/scala/S99/P5.scala
+++ src/main/scala/S99/P5.scala
@@ -1,5 +1,9 @@
 package S99
 
 object P5 {
-  def reverse [T] (list : List[T]): List[T] = list.reverse
+  def reverse [T] (list : List[T]): List[T] = {
+    list.foldLeft(List[T]()) {
+      (acc, i) => i :: acc
+    }
+  }
 }
